656b00701f3e953117a357be32d41c3e69f91ae1
Seth0r <github@seth0r.net>
2019-11-12 18:58:55 +0100

    using env to get console socket

--- docker/qc.sh
+++ docker/qc.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export BASEDIR=${BASEDIR:-/data}
+
 echo "Press CTRL-D to exit."
-socat stdin,raw,echo=0,escape=0x04 "unix-connect:/data/pdmvpn.console"
+socat stdin,raw,echo=0,escape=0x04 "unix-connect:${BASEDIR}/qemu.console"
