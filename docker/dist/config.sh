1ec4c298ee002e51c6f25374665e65eda6105122
Seth0r <github@seth0r.net>
2019-11-12 23:20:22 +0100

    cloning with submodules

--- docker/dist/config.sh
+++ docker/dist/config.sh
@@ -4,4 +4,4 @@ REPOURL=${REPOURL:-https://github.com/seth0r/ffp-pdmsrv}
 cp "${DISTDIR}/runinside.sh" "${TARGETDIR}/root/runinside.sh"
 chmod +x "${TARGETDIR}/root/runinside.sh"
 
-git clone $REPOURL "${TARGETDIR}/root/repo"
+git clone --recursive $REPOURL "${TARGETDIR}/root/repo"
