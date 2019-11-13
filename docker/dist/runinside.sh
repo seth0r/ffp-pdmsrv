0e32ea144a8cda84eff0cff8a23ea923c1c096dd
Seth0r <github@seth0r.net>
2019-11-13 12:50:05 +0100

    reading environment given by docker

--- docker/dist/runinside.sh
+++ docker/dist/runinside.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ -f /root/.inside_qemu ]; then
+    tr -d '\0' < /dev/sdb | sed 's/^/export /g' > /root/env
+    source /root/env
     cd /root/repo
     git pull --recurse-submodules
     make inside
