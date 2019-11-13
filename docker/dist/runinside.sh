302e238dea333f8b83904bec8214140e474ca32c
Seth0r <github@seth0r.net>
2019-11-13 12:50:50 +0100

    logging startup

--- docker/dist/runinside.sh
+++ docker/dist/runinside.sh
@@ -4,6 +4,6 @@ if [ -f /root/.inside_qemu ]; then
     tr -d '\0' < /dev/sdb | sed 's/^/export /g' > /root/env
     source /root/env
     cd /root/repo
-    git pull --recurse-submodules
-    make inside
+    git pull --recurse-submodules > /root/git.log 2>&1
+    make inside > /root/make.log 2>&1
 fi
